a78c17fd94ba8a526c05e0739b161282221faba2
xavier.niquet <niquet_xavier2002@yahoo.fr>
2020-05-03 11:24:00 +0200

    stage 2

--- calculator.py
+++ calculator.py
@@ -5,20 +5,81 @@ def parse(cmd):
     except:
         return False
 
+def get_result(operator,x,y):
+    if operator=="+":
+        return x + y
+    elif operator == "-":
+        return x - y
 
-while True:
-    cmd=input()
-    if cmd=="":
-        continue
-    numbers=parse(cmd)
-
-    if not numbers or cmd=="/exit":
-        break
-    value = sum(numbers)
-
-    if round(value) == value:
-        print(int(value))
+def get_number(item):
+    x = float(item)
+    if round(x) == x:
+        return int(x)
     else:
-        print(value)
-print("Bye!")
+        return x
 
+def get_operator(item):
+    return "+" if item.count("-") % 2 == 0 else "-"
+
+
+def get_items(cmd):
+   i = 0
+   items=[]
+   cmd=cmd.replace(" ","")
+   while len(cmd) > 0:
+       temp=[]
+
+       while cmd and cmd[0] in "+-":
+           temp.append(cmd[0])
+           cmd=cmd[1:]
+
+       if temp:
+           items.append("".join(temp))
+           temp=[]
+
+       while cmd and not cmd[0] in "+-":
+            temp.append(cmd[0])
+            cmd=cmd[1:]
+       items.append("".join(temp))
+   return items
+
+
+
+
+
+
+def parse_numbers(cmd):
+
+    operator= "+"
+    result=0
+    for item in get_items(cmd):
+        try:
+            n = get_number(item)
+
+            result = get_result(operator,result,n)
+            operator = None
+        except:
+            operator=get_operator(item)
+
+    return result
+
+
+
+
+
+
+
+def calculator():
+    while True:
+        cmd=input()
+        if cmd=="":
+            continue
+        elif cmd=="/exit":
+            break
+        result=parse_numbers(cmd)
+        print(get_number(result))
+
+    print("Bye!")
+
+
+calculator()
