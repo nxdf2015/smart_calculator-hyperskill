9238e5aeba5a3447e93ae13ebb615d4245ef0122
xavier.niquet <niquet_xavier2002@yahoo.fr>
2020-05-05 01:03:55 +0200

    final postfix notation

--- calculator.py
+++ calculator.py
@@ -3,6 +3,13 @@ from re import match,compile
 
 variables={}
 
+class InvalidExpression(Exception):
+    pass
+
+class UnknownVariableException(Exception):
+    pass
+
+
 
 def parse(cmd):
     """
@@ -24,6 +31,10 @@ def get_result(operator ,x ,y ):
         return x + y
     elif operator == "-":
         return x - y
+    elif operator=="*":
+        return x * y
+    elif operator=="/":
+        return x / y
 
 
 def get_number(item):
@@ -50,7 +61,8 @@ def get_items(cmd):
    while len(cmd) > 0:
        temp=[]
 
-       while cmd and cmd[0] in "+-":
+
+       while cmd and cmd[0] in "*+-/":
            temp.append(cmd[0])
            cmd=cmd[1:]
 
@@ -58,16 +70,19 @@ def get_items(cmd):
            items.append("".join(temp))
            temp=[]
 
-       while cmd and not cmd[0] in "+-":
+
+       while cmd and not cmd[0] in "+-*/":
             temp.append(cmd[0])
             cmd=cmd[1:]
+
        items.append("".join(temp))
+
    return items
 
 
 def eval_expression(cmd):
     """
-    evaluate expression45
+    evaluate expression
 
     """
     operator= "+"
@@ -88,10 +103,6 @@ def eval_expression(cmd):
             else:
                 operator=get_operator(item)
 
-
-
-
-
     return result
 
 
@@ -101,18 +112,25 @@ def is_assignment(cmd):
 def is_valid_identifier(line):
     return match("[a-zA-Z]+$",line)
 
+def is_number(line):
+    try:
+        float(line)
+        return True
+    except :
+        return False
+
 def is_valid_assignment(line):
-    pattern=r"(\+|-)?\s*((\d+|([a-zA-Z]+)|((\d+|[a-zA-Z]+)\s*[+-]+\s?)+\s*(\d+|[a-zA-Z]+)))$"
-    return match(pattern,line)
-
-
+    """
+    return true if line is a valid expression without parenthesis (stage 3 )
+    """
+    pattern=r"(\+|-|\*|/)?\s*((\d+|([a-zA-Z]+)|((\d+|[a-zA-Z]+)\s*[+\-\*/]+\s?)+\s*(\d+|[a-zA-Z]+)))$"
+    return match(pattern,line) or to_postfix(line)
 
 
 def set_variable(identifier,assignment):
     variables[identifier]=eval_expression(assignment)
 
 def calculator():
-    expression = compile(r"(\+|-)?\s*(\d+|(\d+\s*[+-]+\s?)+\s*\d+)$")
     while True:
         cmd=input().replace(" ","")
 
@@ -138,17 +156,130 @@ def calculator():
         elif cmd.count("=") > 1:
             print("Invalid assignment")
 
-        elif is_valid_assignment(cmd):
-            try:
-                result=eval_expression(cmd)
-                print(get_number(result))
-            except:
-                print("Unknown variable")
-
         else:
-            print("Invalid expression")
+            try:
+                result=eval_infix(cmd)
+                print(get_number(result))
+            except UnknownVariableException:
+                print("Unknown variable")
+            except InvalidExpression:
+                print("Invalid expression")
 
     print("Bye!")
 
-calculator()
 
+def is_value(line):
+    return is_valid_identifier(line) or is_number(line)
+
+def is_operator(line):
+    return line in ["+","-","*","/"]
+
+def compare_precedence(op1,op2):
+    """
+    op1 op2 arihmetic operator
+    return True if op1  has  highter precedence than op2
+        else return False
+
+    """
+    if op1 in "*/":
+        return True
+    else:
+        return False
+
+
+def eval_postfix(expression):
+    stack=[]
+
+    for item in expression.strip().split(" "):
+
+        if is_value(item.strip()):
+            if item in variables:
+                value=variables[item]
+            else:
+                try:
+                    value=int(item)
+                except:
+                    raise UnknownVariableException()
+            stack.append(value)
+
+        elif is_operator(item):
+            *head ,x,y = stack
+            stack=head
+            head.append(get_result(item,x,y))
+
+    return stack.pop()
+
+
+
+def to_postfix(expression):
+    result=""
+    stack=[]
+
+    for item in expression:
+
+        if is_value(item):
+            result+=item
+
+        elif item=="(":
+            stack.append(item)
+
+        elif item==")":
+            valid=False
+
+            if not "(" in stack:
+                raise InvalidExpression()
+            while len(stack)>0:
+                v = stack.pop()
+                if v == "(":
+                    valid=True
+                    break
+                else:
+                    result+=" "+v+" "
+
+            if not valid:
+                raise InvalidExpression()
+
+        elif   is_operator(item):
+
+            result+=" "
+            if not stack:
+                stack.append(item)
+            elif stack[-1]=="(":
+                stack.append(item)
+            elif compare_precedence(item,stack[-1]):
+                stack.append(item)
+            else:
+                while stack and not (stack[-1] == "(" or  compare_precedence(item,stack[-1])):
+                    v=stack.pop()
+                    result+=" "+v+" "
+                stack.append(item)
+
+    if "("in stack:
+        raise InvalidExpression()
+    while stack:
+        result+= " "+stack.pop()
+    return  result
+
+def reduce_expression(expression):
+    """
+    return a reduce valid expression : 1++++2--3+-4  -> 1 + 2 + 3 - 4
+    """
+    operator=compile("[\+\-]+")
+    items=get_items(expression)
+
+    for i,item in enumerate(get_items(expression)):
+        if operator.match(item):
+            items[i]= "+" if item.count("-") % 2 == 0 else "-"
+
+    return "".join(items)
+
+
+
+def eval_infix(expression):
+    """
+    evaluate aithmetic expression with parenthesis
+    """
+    postfix_expression=to_postfix(reduce_expression(expression))
+    return eval_postfix(postfix_expression)
+
+calculator()
